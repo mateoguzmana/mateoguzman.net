a9aa88acd3c4e9262592671c002baf124a99c9e0
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-28 18:51:59 +0200

    feat(links): updating links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/mateoguzmana/mateoguzman.net/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/mateoguzmana/mateoguzman.net/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -71,7 +69,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/mateoguzmana",
             label: "GitHub",
             position: "right",
           },
